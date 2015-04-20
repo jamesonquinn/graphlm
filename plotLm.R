0c1b92bb02f16126aa39e922c985b5fc896789cd
Kyle Mahowald <kmahowald@gmail.com>
2015-04-19 21:58:35 -0400

    minor fix

--- plotLm.R
+++ plotLm.R
@@ -44,7 +44,7 @@ by1var = function(oldLm, var, thin=1) {
   d2 = d2[,c(toString(outcomeVar),"tp_"):=list(adjResid,"adj")]
   bothData = rbind(d1,d2)
   bothData = bothData[,residOfFull:=oldLm$resid + adjustment]
-
+  
   print(outcomeVar)
   print(varName)
   print(names(bothData))
@@ -52,10 +52,10 @@ by1var = function(oldLm, var, thin=1) {
   print(c(1, sapply(newLm$model[,2:dim(newLm$model)[2]],mean)))
   print(head(bothData))
   print(dim(subset(bothData,tp_ == "raw")))
- 
+  
   bothData = data.frame(bothData)
   if (isfactor == FALSE) {  sampled.data = sample(1:nrow(bothData), round(thin * nrow(bothData)) )
-    p <- ggplot(data=bothData[, sampled.data], aes_string(y=toString(outcomeVar),x=varName ))+
+                            p <- ggplot(data=bothData[sampled.data, ], aes_string(y=toString(outcomeVar),x=varName ))+
                               geom_point(data=subset(bothData[sampled.data, ],tp_ == "raw"),alpha=0.3)+
                               geom_point(color="red", data=subset(bothData[sampled.data, ],tp_ == "adj")) + 
                               geom_line(aes(group=gp_), alpha=0.3)+
@@ -65,16 +65,16 @@ by1var = function(oldLm, var, thin=1) {
                               theme_bw()}
   
   else { bothData[, varName] = as.factor(bothData[, varName])
-    p <- ggplot(data=sample_n(bothData, round(thin * nrow(bothData))), aes_string(y=toString(outcomeVar),x=varName ))+
-          geom_boxplot(data=subset(bothData,tp_ == "raw"),alpha=0.3)+
-          geom_boxplot(color="red", data=subset(bothData,tp_ == "adj")) + geom_line(aes(group=gp_), alpha=0.3)+
-          #geom_smooth(method=lm,data=subset(bothData,tp_ == "adj"), color="red", fill='red', alpha=.3, se=F)+
-          #geom_smooth(data=subset(bothData,tp_ == "adj"), size=0, fill = 'blue', color = 'blue', alpha=.2, se=T)+
-          geom_rug(aes(y=residOfFull),data=subset(bothData,tp_ == "adj"),col=rgb(.5,0,0,alpha=.2)) + 
-          theme_bw()
-         }
+         p <- ggplot(data=sample_n(bothData, round(thin * nrow(bothData))), aes_string(y=toString(outcomeVar),x=varName ))+
+           geom_boxplot(data=subset(bothData,tp_ == "raw"),alpha=0.3)+
+           geom_boxplot(color="red", data=subset(bothData,tp_ == "adj")) + geom_line(aes(group=gp_), alpha=0.3)+
+           #geom_smooth(method=lm,data=subset(bothData,tp_ == "adj"), color="red", fill='red', alpha=.3, se=F)+
+           #geom_smooth(data=subset(bothData,tp_ == "adj"), size=0, fill = 'blue', color = 'blue', alpha=.2, se=T)+
+           geom_rug(aes(y=residOfFull),data=subset(bothData,tp_ == "adj"),col=rgb(.5,0,0,alpha=.2)) + 
+           theme_bw()
+  }
   
-
+  
   gcall = quote(p)
   
   #lastcall = eval(bquote(substitute(.(gcall),list(RESPONSE=attributes(terms(formula(oldLm)))$variables[[2]],
@@ -83,7 +83,7 @@ by1var = function(oldLm, var, thin=1) {
   print(gcall)
   eval(gcall)
   return(p)
-  }
+}
 
 dputToString <- function (obj) {
   con <- textConnection(NULL,open="w")
@@ -100,11 +100,11 @@ dputToString <- function (obj) {
 }
 
 by1var.seq <- function(l, thin.val=1) {
-    p.list <- list()
-    vars <- names(l$model)[2:length(names(l$coefficients))]
-    for (i in 1:length(vars)) { p <- by1var(l, vars[i], thin=thin.val)
-      p.list[[i]] <- p}
-    multiplot(plotlist=p.list, cols = 2)}
+  p.list <- list()
+  vars <- names(l$model)[2:length(names(l$coefficients))]
+  for (i in 1:length(vars)) { p <- by1var(l, vars[i], thin=thin.val)
+                              p.list[[i]] <- p}
+  multiplot(plotlist=p.list, cols = 2)}
 
 
 #tests
