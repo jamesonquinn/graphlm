d30a06f72af2debd1a1a88dc988ea63a63328d9d
Kyle Mahowald <kmahowald@gmail.com>
2015-04-22 10:22:53 -0400

    fixed data sampling

--- plotLm.R
+++ plotLm.R
@@ -80,8 +80,10 @@ by1var = function(oldLm, var, thin=1, breakupby=FALSE,
   #print(c(1, sapply(newLm$model[,2:dim(newLm$model)[2]],mean)))
   #print(head(bothData))
   #print(dim(subset(bothData,tp_ == "raw")))
-  sampled.data = sample(1:nrow(bothData), round(thin * nrow(bothData)) )
   bothData = data.frame(bothData)
+  
+  sampled.data = sample(1:max(bothData$gp_), round(thin * max(bothData$gp_)) )
+  #sampled.data = sample(1:nrow(bothData), round(thin * nrow(bothData)) )
   
   title.vars = new.data[1, names(newLm$model)]
   title.vars = title.vars[names(title.vars) %in% breakupby == F & names(title.vars) != toString(outcomeVar)]
@@ -92,11 +94,11 @@ by1var = function(oldLm, var, thin=1, breakupby=FALSE,
   
   if (breakupby == FALSE) {bothData[, 'breakupby'] = 1} else {bothData[, 'breakupby'] = bothData[, breakupby]}
   if (isfactor == FALSE) {  
-     p <- ggplot(data=bothData[sampled.data, ], aes_string(y=toString(outcomeVar),x=varName ))+
+     p <- ggplot(data=bothData[bothData$gp_ %in% sampled.data, ], aes_string(y=toString(outcomeVar),x=varName ))+
           call.with(geom_point,
-                    list(data=subset(bothData[sampled.data, ],tp_ == "raw")), rawData)+
+                    list(data=subset(bothData[bothData$gp_ %in% sampled.data, ],tp_ == "raw")), rawData)+
           call.with(geom_point, 
-                    list(data=subset(bothData[sampled.data, ],tp_ == "adj")), adjustedData) + 
+                    list(data=subset(bothData[bothData$gp_ %in% sampled.data, ],tp_ == "adj")), adjustedData) + 
           call.with(geom_line,list(aes(group=gp_)), connections)+
           call.with(geom_smooth,
                     list(method=lm,data=subset(bothData,tp_ == "adj")), line)+
@@ -106,8 +108,8 @@ by1var = function(oldLm, var, thin=1, breakupby=FALSE,
           theme_bw() + facet_grid(. ~ breakupby) + ggtitle(title.val) + theme(plot.title = element_text(size=10, hjust=1))
   }  else { 
       bothData[, varName] = as.factor(bothData[, varName])
-      p <- ggplot(data=bothData[sampled.data, ], aes_string(y=toString(outcomeVar),x=varName ))+
-           call.with(geom_boxplot,list(data=subset(bothData,tp_ == "raw")),adjustedData)+
+      p <- ggplot(data=bothData[bothData$gp_ %in% sampled.data, ], aes_string(y=toString(outcomeVar),x=varName ))+
+        geom_boxplot(color="black", data=subset(bothData,tp_ == "raw"), alpha=.3)+
            geom_boxplot(color="red", data=subset(bothData,tp_ == "adj")) + geom_line(aes(group=gp_), alpha=0.3)+
            geom_smooth(method=lm,data=subset(bothData,tp_ == "adj"), color="red", fill='red', alpha=.3, se=F)+
            geom_smooth(data=subset(bothData,tp_ == "adj"), size=0, fill = 'blue', color = 'blue', alpha=.2, se=T)+
@@ -161,10 +163,15 @@ by1var.seq <- function(l, ...) {
 
 #tests
 if (dotests) {
-  n = 80
+  
+  
+  n = 160
   x <- data.frame(rmvnorm(n, c(0, 0, 1), .5 * (diag(3) + matrix(.3, 3, 3))))
-  y <- x$X1 + 2*x$X2 + x$X3^2 + rnorm(n,0,.2)
-  l <- lm(y ~ X1 + X2 + X3, data=x)
+  x$X4 <- c(rep(1, n/2), rep(0, n/2))
+  y <- x$X1 + 2*x$X2 + x$X3^2 + rnorm(n,0,.2) + rnorm(n, x$X4, 1)
+  x$X4 <- as.factor(x$X4)
+  l <- lm(y ~ X1 + X2 + X3 + X4 , data=x)
+  plot(l)
   by1var.seq(l)
   
   by1var(l, "X1", thin=1)
@@ -181,12 +188,25 @@ if (dotests) {
   by1var.seq(l)
   dev.off()
   
+  #example with 1
+  x <- rnorm(n)
+  y <- x + rnorm(n)
+  xy <- data.frame(cbind(x, y))
+  xy$z <- rep(0, n)
+  with(xy, plot(x, y))
+  l <- lm(y ~ 1 + x + z, data=xy)
+  by1var(l, 'x') #(l, 'x', 'z')
+  
   l <- lm(mpg ~ wt + hp, data=mtcars)
   by1var(l, "wt")
   
   d <- read.csv('rts.csv')
   d$AgeSubjectNum <- as.numeric(d$AgeSubject)
   d$RTnaming.e = exp(d$RTnaming)
-  l <- lm(RTnaming.e ~ AgeSubject + WrittenFrequency + LengthInLetters + Familiarity, data=d)
+  d$WrittenFrequency.e <- exp(d$WrittenFrequency)
+  l <- lm(RTnaming.e ~ AgeSubject + WrittenFrequency.e + LengthInLetters , data=filter(d, WrittenFrequency.e < 50000))
+  l <- lm(RTnaming.e ~ AgeSubject + WrittenFrequency + LengthInLetters , data=filter(d, WrittenFrequency.e < 50000))
+  l <- lm(RTnaming ~ AgeSubject + WrittenFrequency + LengthInLetters , data=filter(d, WrittenFrequency.e < 50000))
+  
   by1var.seq(l, thin=.1, breakupby='AgeSubject')
 }
