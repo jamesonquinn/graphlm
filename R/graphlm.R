2b245ba6200850536697953ffe4dbcb99f861f11
Jameson Quinn <jameson.quinn+github@gmail.com>
2015-05-04 02:47:28 -0400

    start the documentation

--- R/graphlm.R
+++ R/graphlm.R
@@ -1,3 +1,12 @@
+#' Better diagnostic plots for linear models
+#' @export
+#' @name graphml
+#' @doctype package
+#'
+#' This package makes improved diagnostic plots for linear models.
+#' 
+#' 
+
 library(scales)
 library(ggplot2)
 library(data.table)
@@ -5,7 +14,52 @@ library(dplyr)
 library(gridExtra)
 library(dplyr)
 library(mvtnorm)
-source('multiplot.R')
+
+#' Multiple plot function
+#'
+#' ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+#' @param cols   Number of columns in layout
+#' @param layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#'
+#' If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+#' then plot 1 will go in the upper left, 2 will go in the upper right, and
+#' 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
 
 dotests = F
 
@@ -16,6 +70,22 @@ call.with = function(f,fixedArgs,varArgs,elseResult=geom_blank()) {
   return(elseResult)
 }
 
+#' Diagnostic plot of one variable of a linear model
+#'
+#' This function takes a linear model and creates a plot where the x axis is the given 
+#' variable and the y axis shows the raw data (in gray by default) and the data "adjusted for"
+#' all other variables in the model (in red by default). The adjusted data are the residuals,
+#' recentered at the average value. 
+#' 
+#' @param oldLm   Linear model to diagnose
+#' @param var  string giving name of variable to plot on x. Must be an 
+#'   independent variable of the given linear model.
+#' @param thin   Linear model to diagnose
+#'
+#' If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+#' then plot 1 will go in the upper left, 2 will go in the upper right, and
+#' 3 will go all the way across the bottom.
+#
 by1var = function(oldLm, var, thin=1, breakupby=FALSE, 
                   adjustedData=F, 
                   rawData=F,
